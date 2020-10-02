a7a396dbb8bff2f867c1a1a7d45f7b2e7d86c7f1
Jonathan Rosser <jonathan.rosser@rd.bbc.co.uk>
2020-10-02 07:29:21 +0100

    Fix linter errors
    Change-Id: Iceda4b23bf5347a7de9817d212ca8db8f2befc10

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -230,7 +230,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'doc-' + target_name + '.tex',
-     title.replace("_", "\_"), author, 'manual'),
+     title.replace("_", r"\_"), author, 'manual'),
 ]
 
 latex_use_xindy = False
